a47bf0e01c785159edb498e3bdad06572077c064
Daniel Rogulin <danrogulin@gmail.com>
2026-04-24 05:43:57 +0300

    Added mainpaneviewmodel as base logic

---
+++ src/RevitAddin.Plugin/Views/MainPaneView.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace RevitAddin.Plugin.Views;
+
+public partial class MainPaneView : UserControl {
+    public MainPaneView() {
+        InitializeComponent();
+    }
+}
