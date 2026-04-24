73187d5f52bb2f80dc63d2c1aff9967951c439d9
Daniel Rogulin <danrogulin@gmail.com>
2026-04-24 04:31:01 +0300

    the revit plugin template project has been initialized

---
+++ src/RevitAddin.Plugin/PluginApp.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.UI;
+
+namespace RevitAddin.Plugin;
+
+public sealed class PluginApp : IExternalApplication
+{
+    public Result OnStartup(UIControlledApplication application)
+    {
+        return Result.Succeeded;
+    }
+
+    public Result OnShutdown(UIControlledApplication application)
+    {
+        return Result.Succeeded;
+    }
+}
